2a4bd0f280e649b95cd01909f51390439324d511
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-09 03:41:22 +0000

    added cli as it was not saved

--- lib/p5c/cli.rb
+++ lib/p5c/cli.rb
@@ -5,15 +5,48 @@ require_relative '../scraper'
 
 
 class CLI
+  def option_1
+    print compendium.all_persona_by_name
+    puts "Please select a number 1 to 225!\n"
+    answer = get.strip
+    Persona.all.
+  end
+  def option_2
+    puts "Please enter a Persona's name. If incorrect you will be taken to the list of persona's"
+    answer = get.strip
+    Persona.find_by_name(answer)
+  end
+  def option_3
+    puts "Please input the element."
+    element = gets.strip
+    puts "Please input the effect"
+    effect = gets.strip
+    print Compendium.elements(element,effect)
+  end
+  def option_4
+    puts "Please input the Acrana."
+    tarot = gets.strip
+    Compendium.acrana(tarot)
+  end
+
   def call
-    puts "Welcome to The Person 5 Comdendium! Please allow a moment for the information to be gathered!"
+    puts "Welcome to The Person 5 Comdendium! Please allow a moment for the information to be gathered!\n"
     scrap = Scraper.new
     scrap.get_index_page
     scrap.personal_profile
     scrap.combo_to_make
-    puts "Please select a number from the list of 225 Persona to get their level, acrana, stats, & elements!"
     compendium = Compendium.new
-    print compendium.all_persona_by_name
-    puts "Please select a number 1 to 225!"
+  end
+  def opening
+    puts "What would be best in your search (1,2,3, or 4)?\n 1. To list and pick a number for the Personas information.\n 2. To enter the name to then  view the Personas information.\n 3. Search for all persona by what elements effect them(Ex Arsene is weak against ice).\n 4. Search by acana."
+    search = get.strip
+    if search == "1"
+
+    elsif search == "2"
+
+    elsif search == "3"
+
+    elsif search == "4"
+
   end
 end
